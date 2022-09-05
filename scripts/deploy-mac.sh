cd66bfb2ade66268ffb9711951b3972235c8c69b
Marcus Grant <marcus@odin.local>
2022-09-05 22:02:20 +0200

    Update deploy script

--- scripts/deploy-mac.sh
+++ scripts/deploy-mac.sh
@@ -8,12 +8,20 @@ export PLAYDIR='/tmp/macansible'
 # And make sure that your user has write permission.
 # chmod u+w /opt/homebrew /opt/homebrew/Cellar /opt/homebrew/Frameworks /opt/homebrew/bin /opt/homebrew/etc /opt/homebrew/include /opt/homebrew/lib /opt/homebrew/opt /opt/homebrew/sbin /opt/homebrew/share /opt/homebrew/share/zsh /opt/homebrew/share/zsh/site-functions
 
+export PATH="$HOME/Library/Python/3.8/bin:/opt/homebrew/bin:$PATH"
 xcode-select --install
-sudo pip3 install upgrade pip
+sudo pip3 install --upgrade pip
+sudo pip3 install ansible
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+sudo chown -R $(whoami) /usr/local/Cellar
+sudo chown -R $(whoami) /usr/local/var/homebrew
+sudo chown -R $(whoami) /usr/local/Frameworks /usr/local/Homebrew /usr/local/bin /usr/local/etc /usr/local/include /usr/local/lib /usr/local/opt /usr/local/sbin /usr/local/share /usr/local/share/zsh /usr/local/share/zsh/site-functions
+chmod u+w /usr/local/Frameworks /usr/local/Homebrew /usr/local/bin /usr/local/etc /usr/local/include /usr/local/lib /usr/local/opt /usr/local/sbin /usr/local/share /usr/local/share/zsh /usr/local/share/zsh/site-functions
+/opt/homebrew/bin/brew install git
 mkdir -p $PLAYDIR
 git clone https://github.com/marcus-grant/infra $PLAYDIR
 cd $PLAYDIR
-if [ ! $PLAYDIR in $(which python3) ]; then
+if [ ! $PLAYDIR in "$(which python3)" ]; then
     if [ ! -d $PLAYDIR/venv ]; then
         python3 -m venv $PLAYDIR/venv
     fi
@@ -21,8 +29,9 @@ if [ ! $PLAYDIR in $(which python3) ]; then
     pip install -r $PLAYDIR/requirements.txt
 fi
 
-ansible-galaxy install -r $PLAYDIR/requiremtns.yml
-ansible-playbook mac.yml --filter odin
+ansible-galaxy install -r $PLAYDIR/requirements.yml
+ansible-playbook mac.yml
 
+rm -rf $PLAYDIR
 unset PLAYDIR
 deactivate
