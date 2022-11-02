e83d2e36d43bf72af57edf2e582dd0e47bf27489
Marcus Grant <marcusfg@gmail.com>
2022-11-02 16:44:27 +0100

    Add template for terraform b2 bucket addition

---
+++ tf/b2/bucket-docker-backup.tf
@@ -0,0 +1,22 @@
+# Example from https://registry.terraform.io/providers/Backblaze/b2/latest/docs
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    b2 = {
+      source = "Backblaze/b2"
+    }
+  }
+}
+
+provider "b2" {
+}
+
+resource "b2_application_key" "example_key" {
+  key_name     = "my-key"
+  capabilities = ["readFiles"]
+}
+
+resource "b2_bucket" "example_bucket" {
+  bucket_name = "my-b2-bucket"
+  bucket_type = "allPublic"
+}
